542449ebf9711892d8ad848868be065c05276062
Dan Williams <dan@danwilliams.co>
2015-07-18 09:01:22 +0100

    Add zsh dir and aliases.

---
+++ zsh/aliases.zsh
@@ -0,0 +1,27 @@
+# Xcode
+alias ddd='rm -rf ~/Library/Developer/Xcode/DerivedData'
+alias xc='open *.xcworkspace'
+alias xp='open *.xcproject'
+
+# Editor
+alias s='subl .'
+
+# OS X
+alias o='open .'
+
+# ls
+alias ls='ls -F'
+
+# Git
+alias ga='git add .'
+alias gc='git commit -m'
+alias gac='git commit -am'
+alias gp='git push'
+alias gs='git status'
+
+# Directories
+alias db='cd ~/Dropbox'
+alias proj='cd ~/Dropbox/Projects/Current\ Projects'
+
+# SSH
+alias mediaserver='ssh dan@62.210.85.135'
