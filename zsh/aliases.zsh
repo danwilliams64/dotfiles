6b2b620750a35a8de39006a0c21aed3b927016c8
Dan Williams <danwilliams64@gmail.com>
2016-09-10 09:14:10 +0100

    Remove ssh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -27,7 +27,3 @@ alias db='cd ~/Dropbox'
 alias proj='cd ~/Dropbox/Projects/Current\ Projects'
 alias src='cd ~/source'
 
-# SSH
-alias mediaserver='ssh dan@62.210.85.135'
-alias pi='ssh pi@192.168.0.10'
-alias wall-e='ssh dan@178.62.5.181 -p 4422'
